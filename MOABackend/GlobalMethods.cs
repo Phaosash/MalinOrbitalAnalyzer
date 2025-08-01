2f842639e00397816de0d512d33f38dfd5c64107
Phaosash <michael_mckie@hotmail.com>
2025-08-01 15:41:33 +0800

    Completed UI Framework

---
+++ MOABackend/GlobalMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galileo6;
+
+namespace MOABackend;
+
+public partial class GlobalMethods {
+    //  Programming requirements 4.1
+    private LinkedList<double> _sensorA = new();
+    private LinkedList<double> _sensorB = new();
+
+    //  Programming requirements 4.2
+    public void LoadData (){
+
+    }
+}
---
+++ MalinOrbitalAnalyzer/DisplayElements/CombinedData.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MalinOrbitalAnalyzer.DisplayElements;
+
+/// <summary>
+/// Interaction logic for CombinedData.xaml
+/// </summary>
+public partial class CombinedData : UserControl {
+    public CombinedData (){
+        InitializeComponent();
+    }
+}
