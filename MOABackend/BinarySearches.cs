7c748f5190c391fb5303d2ab37b0f19c2791848a
Phaosash <michael_mckie@hotmail.com>
2025-08-08 15:40:29 +0800

    Got both lists displaying in the listview

---
+++ MOABackend/BinarySearches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOABackend;
+
+internal class BinarySearches {
+    public static int BinarySearchIterative(LinkedList<double> list, double searchValue, int minimum, int maximum) {
+        List<double> sortedList = list.ToList();
+
+        while (minimum <= maximum - 1){
+            int middle = (minimum + maximum) / 2;
+            double middleValue = sortedList[middle];
+            double epsilon = 0.00001;
+
+            if (Math.Abs(searchValue - middleValue) < epsilon){
+                return middle + 1;
+            }
+            else if (searchValue < middleValue){
+                maximum = middle - 1;
+            } else 
+            {
+                minimum = middle + 1;
+            }
+        }
+
+        return minimum;
+    }
+}
